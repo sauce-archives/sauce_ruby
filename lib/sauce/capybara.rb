b0eca1518acf20b32d592209d72b327aa33af81c
Eric Allen <ericpallen@gmail.com>
2011-02-18 15:48:30 -0800

    Significantly improved Capybara support

--- lib/sauce/capybara.rb
+++ lib/sauce/capybara.rb
@@ -5,10 +5,15 @@ module Sauce
     class Driver < ::Capybara::Driver::Selenium
       def browser
         unless @browser
+          puts "[Connecting to Sauce OnDemand...]"
           config = Sauce::Config.new
-          @sauce_tunnel = Sauce::Connect.new(:host => "127.0.0.1", :port => rack_server.port)
+          @domain = "#{rand(10000)}.test"
+          @sauce_tunnel = Sauce::Connect.new(:host => "127.0.0.1",
+                                             :port => rack_server.port,
+                                             :domain => @domain,
+                                             :quiet => true)
           @sauce_tunnel.wait_until_ready
-          @browser = Sauce::Selenium2.new(:name => "Capybara")
+          @browser = Sauce::Selenium2.new(:name => "Capybara", :browser_url => "http://#{@domain}")
           at_exit do
             @browser.quit
             @sauce_tunnel.disconnect
@@ -23,8 +28,7 @@ module Sauce
         if path =~ /^http/
           path
         else
-          config = Sauce::Config.new
-          config.browser_url + path.to_s
+          "http://#{@domain}#{path}"
         end
       end
     end
@@ -33,4 +37,12 @@ end
 
 Capybara.register_driver :sauce do |app|
   Sauce::Capybara::Driver.new(app)
+end
+
+# Switch Cucumber stories tagged with @selenium to use sauce
+begin
+  Before("@selenium") do
+    Capybara.current_driver = :sauce
+  end
+rescue NoMethodError
 end
