015a17e44a5d13fb8853577a91a274a98b20ec94
Eric Allen <ericpallen@gmail.com>
2011-03-21 20:05:31 -0700

    Fix bug

--- lib/sauce/selenium.rb
+++ lib/sauce/selenium.rb
@@ -2,7 +2,7 @@ require "selenium/client"
 require "selenium/webdriver"
 
 module Sauce
-  class Selenium < Selenium::Client::Driver
+  class Selenium < ::Selenium::Client::Driver
     def initialize(opts={})
       @config = Sauce::Config.new(opts)
       super(opts.merge({:host => @config.host, :port => @config.port,
@@ -13,7 +13,7 @@ module Sauce
   class Selenium2
     def initialize(opts={})
       @config = Sauce::Config.new(opts)
-      http_client = Selenium::WebDriver::Remote::Http::Default.new
+      http_client = ::Selenium::WebDriver::Remote::Http::Default.new
       http_client.timeout = 300 # Browser launch can take a while
       @driver = ::Selenium::WebDriver.for(:remote, :url => "http://#{@config.username}:#{@config.access_key}@#{@config.host}:#{@config.port}/wd/hub", :desired_capabilities => @config.to_desired_capabilities, :http_client => http_client)
       http_client.timeout = 90 # Once the browser is up, commands should time out reasonably
