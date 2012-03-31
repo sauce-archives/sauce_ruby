0a59125e2bcab154fbc3ad738350127accd744d0
R. Tyler Croy <tyler@monkeypox.org>
2012-03-30 20:11:47 -0700

    Prune the old Sauce::Selenium2 test case

--- test/test_selenium2.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path("../helper", __FILE__)
-
-class TestSelenium2 < Test::Unit::TestCase
-  def test_successful_connection_from_environment
-    selenium = Sauce::Selenium2.new(:job_name => "Sauce gem test suite: test_selenium2.rb")
-    selenium.navigate.to "http://www.example.com/"
-    selenium.quit
-  end
-end
