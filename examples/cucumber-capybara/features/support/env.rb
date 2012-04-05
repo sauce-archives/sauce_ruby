5ba23761256b997a59e8e32740db330d705b99d6
R. Tyler Croy <tyler@monkeypox.org>
2012-04-05 00:33:07 -0700

    Refactor the capybara+cucumber support into a separate file and properly set the job name.
    Fixes #60

--- examples/cucumber-capybara/features/support/env.rb
+++ examples/cucumber-capybara/features/support/env.rb
@@ -1,5 +1,6 @@
 
 require 'capybara/cucumber'
 require 'sauce/capybara'
+require 'sauce/capybara/cucumber'
 
 Capybara.current_driver = :sauce
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sauce/capybara.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/sauce/capybara/cucumber.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sauce/connect.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sauce/config.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/sauce/integrations.rb')
