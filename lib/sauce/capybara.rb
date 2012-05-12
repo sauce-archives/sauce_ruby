d01c158556b817fcbdae7fbce1672161464c314e
R. Tyler Croy <tyler@monkeypox.org>
2012-05-12 15:20:38 -0700

    Refactor the RetryableDriver code up into Sauce::Capybara::Driver
    This way the retryable functionality will be enabled by default, and hopefully
    the "standard" driver used will be very reliable!

--- lib/sauce/capybara.rb
+++ lib/sauce/capybara.rb
@@ -21,6 +21,34 @@ module Sauce
     module_function :connect_tunnel
 
     class Driver < ::Capybara::Selenium::Driver
+      RETRY_ON = [::Selenium::WebDriver::Error::UnhandledError,
+                  ::Selenium::WebDriver::Error::UnknownError]
+      MAX_RETRIES = 3
+
+      def handle_retry(method, *args)
+        retries = 0
+        begin
+          send("base_#{method}".to_sym, *args)
+        rescue *RETRY_ON => e
+          if retries < MAX_RETRIES
+            puts "Received an exception (#{e}), retrying"
+            retries = retries + 1
+            retry
+          else
+            raise
+          end
+        end
+      end
+
+      alias :base_find :find
+      alias :base_visit :visit
+      alias :base_current_url :current_url
+
+      [:find, :visit, :current_url].each do |method|
+        define_method(method) do |*args|
+          handle_retry(method, *args)
+        end
+      end
       def browser
         unless @browser
           if Sauce.get_config[:start_tunnel]
--- spec/sauce/capybara_spec.rb
+++ spec/sauce/capybara_spec.rb
@@ -55,7 +55,47 @@ describe Sauce::Capybara do
         end
       end
     end
+    context 'with a mock app' do
+      let(:app) { double('Mock App for Driver') }
 
+      subject do
+        Sauce::Capybara::Driver.new(app)
+      end
+
+      describe '#find' do
+        let(:selector) { '#lol' }
+
+        it 'should route through handle_retry' do
+          subject.should_receive(:base_find).with(selector) # BLECH
+          subject.find(selector)
+        end
+
+        it 'should retry 3 times and then raise' do
+          subject.should_receive(:base_find).with(selector).exactly(4).times.and_raise(Selenium::WebDriver::Error::UnknownError)
+
+          expect {
+            subject.find(selector)
+          }.to raise_error(Selenium::WebDriver::Error::UnknownError)
+        end
+      end
+
+      describe '#visit' do
+        it 'should route through #handle_retry' do
+          path = '/lol'
+          subject.should_receive(:base_visit).with(path)
+          subject.visit(path)
+        end
+      end
+
+      describe '#current_url' do
+        it 'should route through #handle_retry' do
+          url = 'http://lol'
+          subject.should_receive(:base_current_url).and_return(url)
+          subject.current_url.should == url
+        end
+      end
+
+    end
   end
 
   describe '#install_hooks' do
