aba6c8bdc69bfe1d2595ea3bdcabf6767063416a
R. Tyler Croy <tyler@monkeypox.org>
2012-08-11 15:56:58 -0700

    Relocate the sauce-jasmine code under the appropriate peripheral gem

--- gems/sauce-jasmine/lib/sauce-jasmine.rb
+++ gems/sauce-jasmine/lib/sauce-jasmine.rb
@@ -1,5 +1,35 @@
+require 'jasmine'
+
 module Sauce
   module Jasmine
-    # Your code goes here...
+    class Driver < ::Jasmine::SeleniumDriver
+      attr_reader :http_address, :driver, :browser
+
+      def initialize(browser, http_address)
+        @browser = browser
+        @http_address = http_address
+        @driver = Sauce::Selenium2.new(:browser => browser, :job_name => job_name)
+        puts "Starting job named: #{job_name}"
+      end
+
+      def job_name
+        "Jasmine Test Run #{Time.now.utc.to_i}"
+      end
+    end
   end
 end
+
+module Jasmine
+  class Config
+    def jasmine_port
+      '3001'
+    end
+
+    def start
+      @client = Sauce::Jasmine::Driver.new(browser, "#{jasmine_host}:#{jasmine_port}/")
+      @client.connect
+    end
+  end
+end
+
+
