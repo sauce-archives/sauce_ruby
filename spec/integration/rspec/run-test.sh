3b4d9a21c6ae1ad018675361925f2deadaad50fd
Dylan Lacey <github@dylanlacey.com>
2013-05-15 13:42:00 -0700

    Clean up typos from last commit

--- spec/integration/rspec/run-test.sh
+++ spec/integration/rspec/run-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 RAKE="bundle exec rake"
 
--- spec/sauce/test_unit_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-describe "Sauce::Test::Unit" do
-
-  it "should add a driver to the pool" do
-
-  end
-end
