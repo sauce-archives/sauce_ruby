3a1198d9ca629def69a201b8e3e0cdc780dad4f2
Dylan Lacey <github@dylanlacey.com>
2013-05-30 00:42:22 -0700

    Wrap at_exit block to correct exit codes being reset to 0
    Ruby 1.9.3 with patchlevel lower then 382 does not deal with nested at_exit codes

--- lib/sauce.rb
+++ lib/sauce.rb
@@ -5,3 +5,17 @@ require 'sauce/client'
 require 'sauce/config'
 require 'sauce/selenium'
 require 'sauce/integrations'
+
+# Ruby before 1.9.3-p382 does not handle exit codes correctly when nested
+if RUBY_VERSION == "1.9.3" && RUBY_PATCHLEVEL < 392
+  module Kernel
+    alias :existing_at_exit :at_exit
+    def at_exit(&block)
+      existing_at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
