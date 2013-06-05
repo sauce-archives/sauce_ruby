f1277381cd03e21fd737792c0feb43db244181f5
Dylan Lacey <github@dylanlacey.com>
2013-06-05 03:11:07 -0700

    Make options stand alone for each || Process
    The options hash passed to the rspec runner could potentially cross threads and end up with the wrong env_vars.  So fix this by duplicating it on thread entry.
    
    Bump version.

--- lib/parallel_tests/saucerspec/runner.rb
+++ lib/parallel_tests/saucerspec/runner.rb
@@ -6,13 +6,14 @@ module ParallelTests
     class Runner < ParallelTests::RSpec::Runner
 
       def self.run_tests(test_files, process_number, num_processes, options)
+        our_options = options.dup
         exe = executable # expensive, so we cache
         version = (exe =~ /\brspec\b/ ? 2 : 1)
-        cmd = [exe, options[:test_options], (rspec_2_color if version == 2), spec_opts, *test_files].compact.join(" ")
+        cmd = [exe, our_options[:test_options], (rspec_2_color if version == 2), spec_opts, *test_files].compact.join(" ")
         env = Sauce::TestBroker.next_environment(test_files)
         env << " #{rspec_1_color}" if version == 1
-        options = options.merge(:env => env)
-        execute_command(cmd, process_number, num_processes, options)
+        our_options.merge!(:env => env)
+        execute_command(cmd, process_number, num_processes, our_options)
       end
 
 
--- lib/sauce/version.rb
+++ lib/sauce/version.rb
@@ -1,6 +1,6 @@
 module Sauce
   MAJOR_VERSION = '3.0'
-  PATCH_VERSION = '0.beta.2'
+  PATCH_VERSION = '0.beta.3'
 
   def version
     "#{MAJOR_VERSION}.#{PATCH_VERSION}"
