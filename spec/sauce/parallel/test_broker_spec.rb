9a80f6d43c6f7ad6fa2210e767423dbe7fe3e5e6
Steven Hazel <sah@saucelabs.com>
2013-06-06 15:02:31 -0700

    bugfix, and test multi-browser environments

--- spec/sauce/parallel/test_broker_spec.rb
+++ spec/sauce/parallel/test_broker_spec.rb
@@ -15,7 +15,7 @@ describe Sauce::TestBroker do
       end
     end
 
-    it "returns the first environment for new entries" do
+    it "returns the first browser for new entries" do
       first_environment = Sauce::TestBroker.next_environment ["spec/a_spec"]
       first_environment.should eq({
         :SAUCE_PERFILE_BROWSERS => (
@@ -27,19 +27,36 @@ describe Sauce::TestBroker do
       })
     end
 
-    it "should only return an environment once" do
-      Sauce::TestBroker.next_environment "spec/b_spec"
+    it "only returns a browser once for a given file" do
+      Sauce::TestBroker.next_environment ["spec/a_spec"]
       second_environment = Sauce::TestBroker.next_environment ["spec/a_spec"]
 
       second_environment.should eq({
         :SAUCE_PERFILE_BROWSERS => (
           "'" +
           JSON.generate({"./spec/a_spec" => [{"os" => "Linux",
-                                               "browser" => "Firefox",
-                                               "version" => "19"}]}) +
+                                              "browser" => "Firefox",
+                                              "version" => "19"}]}) +
           "'")
       })
     end
+
+    it "returns multiple browsers for files given multiple times" do
+      first_environment = Sauce::TestBroker.next_environment ["spec/a_spec",
+                                                              "spec/a_spec"]
+      first_environment.should eq({
+        :SAUCE_PERFILE_BROWSERS => (
+          "'" +
+          JSON.generate({"./spec/a_spec" => [{"os" => "Windows 7",
+                                              "browser" => "Opera",
+                                              "version" => "10"},
+                                             {"os" => "Linux",
+                                              "browser" => "Firefox",
+                                              "version" => "19"}]}) +
+          "'")
+      })
+    end
+
   end
 
   describe "#test_platforms" do
