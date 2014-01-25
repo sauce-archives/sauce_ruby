bf1fa72a9431718ac8de026565ef71071e6ea180
Isaac Murchie <isaac@saucelabs.com>
2014-01-24 18:29:10 -0800

    Don't access @opts bare

--- lib/sauce/config.rb
+++ lib/sauce/config.rb
@@ -122,12 +122,12 @@ module Sauce
         warn "[DEPRECATED] This method (#{meth}) is deprecated, please use the [] and []= accessors instead"
       end
       if meth.to_s =~ /(.*)=$/
-        @opts[$1.to_sym] = args[0]
+        self[$1.to_sym] = args[0]
         return args[0]
       elsif meth.to_s =~ /(.*)\?$/
-        return @opts[$1.to_sym]
+        return self[$1.to_sym]
       else
-        return @opts[meth]
+        return self[meth]
       end
     end
 
