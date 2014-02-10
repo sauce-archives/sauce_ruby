99ca4ab9508b0974025bc047fc088660ebaa8777
Dylan Lacey <github@dylanlacey.com>
2014-02-10 00:07:08 -0800

    Try to allow for webmock connections to Sauce's infrastructure

---
+++ lib/sauce/webmock.rb
@@ -0,0 +1,16 @@
+require 'webmock/config'
+
+config = WebMock::Config.instance
+
+unless config.allow_net_connect
+  allow_localhost = config.allow_localhost
+  allow = config.allow || []
+  allow << /ondemand.saucelabs.com/
+  connect_on_start = config.net_http_connect_on_start
+
+  WebMock.disable_net_connect!(
+    :allow_localhost => allow_localhost,
+    :allow => allow, 
+    :net_http_connect_on_start => connect_on_start
+  )
+end
